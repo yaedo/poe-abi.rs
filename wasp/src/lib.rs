3b7e093bf97b2b1a3582eea59d379667f9b17ee3
Cameron Bytheway <bytheway.cameron@gmail.com>
2018-12-20 10:30:53 -0700

    add WaspAssert trait

--- wasp/src/lib.rs
+++ wasp/src/lib.rs
@@ -1,2 +1,40 @@
 pub use wasp_core::*;
 pub use wasp_http as http;
+
+pub trait WaspAssert<T> {
+    fn assert(self, message: &str) -> T;
+}
+
+impl<T> WaspAssert<T> for Option<T> {
+    fn assert(self, message: &str) -> T {
+        match self {
+            Some(v) => v,
+            None => {
+                wasp_core::log::write(wasp_core::log::Level::Error, message).unwrap();
+                panic!()
+            }
+        }
+    }
+}
+
+impl<T, E: std::fmt::Debug> WaspAssert<T> for Result<T, E> {
+    fn assert(self, message: &str) -> T {
+        match self {
+            Ok(v) => v,
+            Err(e) => {
+                wasp_core::log::write(
+                    wasp_core::log::Level::Error,
+                    &format!("{:?}: {}", e, message),
+                )
+                .unwrap();
+                panic!()
+            }
+        }
+    }
+}
+
+pub mod dev {
+    pub fn inspect<T: std::fmt::Debug>(data: T) {
+        wasp_core::log::write(wasp_core::log::Level::Debug, &format!("{:?}", data)).unwrap();
+    }
+}
